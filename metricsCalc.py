d249590125ece68ff25cb0fe8c8c8e4002f5bde1
thomas liebgott <thomas.liebgott@live.fr>
2023-06-12 19:31:46 +0200

    stabil problem with metricCalcs

--- metricsCalc.py
+++ metricsCalc.py
@@ -17,9 +17,10 @@ model_ft.fc = nn.Linear(num_ftrs, 7) # chnage if it's another last layer type
 fileNameModel = "test"
 
 # load the model 
-model_ft.load_state_dict(torch.load(r"C:\Users\Thomaslieb\Desktop\PML\output\model\model_resnet50_25epochs_dx4\model.pth"))
+model_ft.load_state_dict(torch.load(r"D:\PML\Project_PML\output\model\model_resnet50_25epochs_dx4\model.pth"))
 model_ft = model_ft.to(device)
 model_ft.eval()
+
 
 data_transforms = {
     'test': transforms.Compose([
@@ -30,7 +31,7 @@ data_transforms = {
     ])
 }
 
-data_dir = r'D:\PML\Project_PML\dx4_ohneHaareEntfernung'
+data_dir = r'dx4_ohneHaareEntfernung'
 
 image_dataset = datasets.ImageFolder(os.path.join(data_dir, 'test'), data_transforms['test'])
 
@@ -38,6 +39,7 @@ dataloader = torch.utils.data.DataLoader(image_dataset, batch_size=4, shuffle=Tr
 
 true_labels = []
 predicted_labels = []
+
 
 if __name__ == '__main__':
     model_ft.eval()  # load the model and setup in evaluate mode
